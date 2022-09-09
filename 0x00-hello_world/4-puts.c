b9f6f48c307455ce46d980e97514bc89292e3071
Isirameroy <isirameroy@gmail.com>
2022-09-08 21:59:59 -0700

    4.HELLO,PUTS

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - printprint the string in the put function
+*
+* Discription:using the main function
+* this program prints "programming is like building a multiligua puzzle
+* Return: 0
+*/
+int main(void)
+{
+         puts("\"programming is like building a multiligua puzzle");
+         return (0);
+}
