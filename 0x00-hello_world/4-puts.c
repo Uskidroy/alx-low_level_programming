dbb909c5f8cb3bd2f0d3c3ec870d14bdef15a463
Isirameroy <isirameroy@gmail.com>
2022-09-11 17:03:51 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
 * main - print the string in the put function
 *
-* Discription:using the main function
-* this program prints "programming is like building a multiligaul puzzel
-* Return: 0
+*Discription: using the main function
+*this program prints "programming is like building a multiligaul puzzle
+*Return: 0
 */
 int main(void)
 {
-	puts("\"programming is like building a multiligaul puzzle");
-	return (0)
+        puts("\"programming is like building a multiligaul puzzle");
+        return (0);
 }
