7bd41a781ff918dbe55e6c0ffe8e083abdd21740
Isirameroy <isirameroy@gmail.com>
2022-09-11 18:34:21 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
- #include <stdio.h>
- /**
- * main - print the string in the put function
- * this program prints "progamming is like building a multiligual puzzle
- * Return: (0)
- */
+#include <stdio.h>
+
+* main - print the spring in the main function
+*
+* Discription: using the main function
+* this program prints "programming is like building a multiligual puzzle
+* Return: 0
+*/
 int main(void)
 {
-        puts("\"Programming is like building a multiligual puzzle");     
-	return (0);
+	puts("\programming is like building a multiligual puzzle");
+	return (0)
 }
